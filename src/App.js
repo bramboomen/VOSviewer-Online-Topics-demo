0c4c39aa969fbdd7bb29e33c0cb6618effed768a
Bram van den Boomen <bram.boomen@gmail.com>
2024-04-03 17:26:59 +0200

    implementation of scores and mui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Autocomplete, Table, TableBody, TableContainer, TableHead, TableRow, TableCell, TextField, Button, LinearProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { VOSviewerOnline } from "vosviewer-online";
 import landscape_data from "./data/openalex_2023nov.json";
@@ -42,122 +43,146 @@ const getTopics = async (filter="") => {
 };
 
 // Enrich the data with the topics from the OpenAlex API
-const addTopics = (data, topics={}) => {
-    if (Object.keys(topics).length === 0) return data;
+const addTopics = (data, topics, alltopics) => {
     // create a new object, otherwise react will not register the state-change
     const combined_data = JSON.parse(JSON.stringify(data))
     combined_data.network?.items?.forEach((item) => {
-        item.weights["OpenAlex Topic Count"] = topics[topicOAid(item.id)]?.count || 0;
+        const count = topics[topicOAid(item.id)]?.count || 0
+        const allcount = alltopics[topicOAid(item.id)]?.count
+        item.scores["OpenAlex Topic P"] = allcount > 0 ? count / allcount : 0 || 0
+        item.weights["OpenAlex Topic Count"] = count || 0
     });
     return combined_data
-};
+}
 
-const VosViewer = ({data, topics}) => {
+const VosViewer = ({data}) => {
     return (
         <div style={{width: "1100px", height: "700px", border: "lightpink solid 5px"}}>
-            <VOSviewerOnline data={addTopics(data, topics)} parameters={{ scale: 1.0, item_size: 2 }} />
+            <VOSviewerOnline data={data} parameters={{ scale: 1.0, item_size: 2 }} />
         </div>
     )
 }
 
-const TopicsTable = ({data, topics}) => {
-    const combined_data = addTopics(data, topics)
+const TopicsTable = ({data}) => {
     return (
-        <table style={{ margin: 10 }}>
-            <thead>
-                <tr>
-                    <td><b>ID</b></td>
-                    <td><b>Label</b></td>
-                    <td><b>Count</b></td>
-                </tr>
-            </thead>
-            <tbody>
-                {combined_data.network.items
-                    .sort((item1, item2) => item1.weights["OpenAlex Topic Count"] < item2.weights["OpenAlex Topic Count"])
-                    .map((item) => {
-                        return (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.label}</td>
-                                <td>{item.weights["OpenAlex Topic Count"]}</td>
-                            </tr>
-                        );
-                    })}
-            </tbody>
-        </table>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell align="right">Label</TableCell>
+                        <TableCell align="right">Count</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.network.items
+                        .sort((item1, item2) => item1.weights["OpenAlex Topic Count"] < item2.weights["OpenAlex Topic Count"])
+                        .map((item) => {
+                            return (
+                                <TableRow key={item.id}>
+                                    <TableCell>{item.id}</TableCell>
+                                    <TableCell align="right">{item.label}</TableCell>
+                                    <TableCell align="right">{item.weights["OpenAlex Topic Count"]}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
 const InstitutionsSelector = ({institution, setInstitution}) => {
     const [input, setInput] = useState("")
-    const [suggestions, setSuggestions] = useState([])
+    const [suggestions, setSuggestions] = useState({})
 
     useEffect(() => {
         const autocomplete = async (input) => {
             const results = await OpenAlexApi("autocomplete/institutions", `q=${input}`)
-            setSuggestions(results)
-            setInstitution(results[0]?.id)
+            const suggestions_obj = results.reduce((results_obj, result) => {
+                results_obj[result.display_name] = result;
+                return results_obj;
+            }, {});
+            setSuggestions(suggestions_obj)
         }
-        if (input) {
-            autocomplete(input)
-        } else {
-            setSuggestions([])
-            setInstitution("")
-        }
+        autocomplete(input)
     }, [input]);
 
     return (
         <>
-            <input defaultValue="input" value={input} onChange={e => setInput(e.target.value)} />
-            <button key="clear" onClick={() => setInput("")}>Clear</button>
+            <Autocomplete
+                freeSolo
+                autoHighlight
+                autoSelect
+                id="institutionselector"
+                options={ Object.keys(suggestions) }
+                onChange={(_event, value) => setInstitution(suggestions[value]?.id)}
+                inputValue={input}
+                onInputChange={(_event, value) => setInput(value)}
+                renderInput={(params) => <TextField {...params} label="Institution" />}
+            />
             <pre>Selected: {institution}</pre>
-            <ul>
-                { suggestions?.map(s => <li key={s.id} style={{ cursor: "pointer" }}><pre onClick={() => setInput(s.display_name)}>{s.display_name.slice(0, 40)}</pre></li>) }
-            </ul>
         </>
     )
 }
 
 const App = () => {
+    const [data, setData] = useState(landscape_data);
+
     const [topics, setTopics] = useState({});
     const [institution, setInstitution] = useState("")
+    const [institutionTopics, setInstitutionTopics] = useState({});
 
     const [key, setKey] = useState(0);
-    const [load, setLoad] = useState(false);
     const [loading, setLoading] = useState(false);
 
     // Collect the topics from the OpenaAlex API
     // Needs to be in a useEffect block because `fetch` is async
     useEffect(() => {
         const getTopicsAsync = async () => {
-            var filter = ""
-            if (institution) { filter += `authorships.institutions.id:${institution}`}
             setLoading(true)
-            setTopics(await getTopics(filter ? `filter=${filter}` : ""));
+            const results = await getTopics()
+            setTopics(results);
+            setInstitutionTopics(results);
             setLoading(false)
         };
-        if (load) {
-            getTopicsAsync();
-            setLoad(false);
+        getTopicsAsync();
+    }, []);
+
+    // Collect the topics from the OpenaAlex API
+    // Needs to be in a useEffect block because `fetch` is async
+    useEffect(() => {
+        const getTopicsAsync = async () => {
+            setLoading(true)
+            setInstitutionTopics(await getTopics(`filter=authorships.institutions.id:${institution}`));
+            setLoading(false)
+        };
+        if (institution) {
+            getTopicsAsync()
         }
-    }, [load]);
+    }, [institution]);
+
+    useEffect(() => {
+        setData(data => addTopics(data, institutionTopics, topics))
+    }, [institutionTopics, topics])
 
     // Make sure the VOSviewerOnline component is re-created when the data is updated
     useEffect(() => {
         setKey((key) => key + 1);
-    }, [topics]);
+    }, [data]);
 
     return (
         <>
-            <VosViewer key={`vos${key}`} data={landscape_data} topics={topics}/>
+            <VosViewer key={`vos${key}`} data={data}/>
             <div style={{ margin: 10 }}>
                 <InstitutionsSelector institution={institution} setInstitution={setInstitution} />
             </div>
             <div style={{ margin: 10 }}>
-                <button onClick={ () => setLoad(true) }>Load topics</button>
-                <pre>{loading ? "Loading topics..." : `${Object.keys(topics).length} Topics loaded`}</pre>
+                <pre>{`${Object.keys(topics).length} topics loaded`}</pre>
+                <pre>{`${Object.keys(institutionTopics).length} institution topics loaded`}</pre>
             </div>
-            <TopicsTable data={landscape_data} topics={topics} />
+            { loading ? <LinearProgress /> : <LinearProgress variant="determinate" value={Object.keys(topics).length > 0 ? 100 : 0} /> }
+            <TopicsTable data={data} />
         </>
     );
 };
